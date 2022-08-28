ae61b2399207f1ce6475ea06026755de92ac1e69
JinhuiKim <public.jinhui.kim@gmail.com>
2022-08-29 07:19:17 +0900

    [BOJ] 1000 A+B
    입출력과 사칙연산

---
+++ baekjoon-online-judge/1주차/김진희/03_1000.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ");
+
+const [a, b] = input;
+
+console.log(Number(a) + Number(b));
