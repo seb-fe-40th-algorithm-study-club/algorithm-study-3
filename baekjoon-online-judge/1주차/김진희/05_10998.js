098b4cdc5f42bc88319d662e79ea6c3ede8f40a3
JinhuiKim <public.jinhui.kim@gmail.com>
2022-08-29 07:23:29 +0900

    [BOJ] 10998 A×B
    입출력과 사칙연산

---
+++ baekjoon-online-judge/1주차/김진희/05_10998.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ");
+
+const [a, b] = input.map((ele) => Number(ele));
+
+console.log(a * b);
