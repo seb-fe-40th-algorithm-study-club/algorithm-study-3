8f788387d24671e83ab7c75c08b7be0cff9c2159
Yerin Kim <ggg06141@gmail.com>
2022-09-17 07:49:29 +0900

    코드 수정

--- baekjoon-online-judge/4주차/김예린/10818_최소, 최대.js
+++ baekjoon-online-judge/4주차/김예린/10818_최소, 최대.js
@@ -1,7 +1,6 @@
 let fs = require('fs')
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-let a = +input[0]
 let list = input[1].split(' ').map(Number)
 
 console.log(`${Math.min(...list)} ${Math.max(...list)}`)
