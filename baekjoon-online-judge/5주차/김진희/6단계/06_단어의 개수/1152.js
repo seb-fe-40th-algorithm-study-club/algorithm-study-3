a17299b464a403e33086a869508ad2a2d8f5b5c2
JinhuiKim <public.jinhui.kim@gmail.com>
2022-09-23 22:40:41 +0900

    [BOJ] 1152 단어의 개수
    문자열

---
+++ baekjoon-online-judge/5주차/김진희/6단계/06_단어의 개수/1152.js
@@ -0,0 +1,13 @@
+// 틀림
+/*
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ");
+console.log(input.length);
+*/
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ");
+
+console.log(input[0] === "" ? 0 : input.length);
