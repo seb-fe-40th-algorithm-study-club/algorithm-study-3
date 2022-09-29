dd92eb9a45db41557213cd57e3380be718d69646
seoyeon <seoyeon@Codeui-MacBookPro.local>
2022-09-29 23:31:29 +0900

    1,4번 해결

---
+++ baekjoon-online-judge/5주차/김서연/1157.js
@@ -0,0 +1,23 @@
+const input = require('fs').readFileSync("../예제.txt").toString().toLowerCase();
+// 제출전 경로 수정하기 '/dev/stdin'
+
+//답 찾아봤습니다 ㅠ
+const result = new Array(26).fill(0);
+
+for (let i = 0; i < input.length; i++) {
+  result[input.charCodeAt(i) - 97] ++;
+}
+
+const max = Math.max(...result);
+const index = result.indexOf(max);
+
+let isSame = false;
+
+for (let j = 0; j < 26; j++) {
+  if (result[j] === max && index != j) {
+    isSame = true;
+    break;
+  }
+}
+
+console.log(isSame ? "?" : String.fromCharCode(index + 65));
