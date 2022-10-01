43f482489a9c7c5ad171dda34c403f29025c36f4
seoyeon <seoyeon@Codeui-MacBookPro.local>
2022-10-01 17:16:08 +0900

    재정비

--- baekjoon-online-judge/6주차/김서연/1427.js
+++
@@ -1,9 +0,0 @@
-const input = require('fs').readFileSync("../../예제.txt").toString().trim().split('').map(Number);
-// 제출전 경로 수정하기 '/dev/stdin'
-let result = input.sort((a,b)=> b-a);
-let sum = '';
-
-for(let i = 0; i < result.length; i++){
-    sum = sum + input[i];
-}
-console.log(sum);
