88bcdf2c324f861cf1fe9bf2613b58833d36d2db
JinhuiKim <public.jinhui.kim@gmail.com>
2022-10-03 22:41:17 +0900

    [BOJ] 18870 좌표 압축
    정렬

--- baekjoon-online-judge/6주차/김진희/9단계/11_좌표 압축/18870.js
+++ baekjoon-online-judge/6주차/김진희/9단계/11_좌표 압축/18870.js
@@ -4,11 +4,11 @@ let [N, input] = fs.readFileSync(filePath).toString().trim().split("\n");
 let answer = input.split(" ");
 const set = new Set(answer);
 
-const array = [...set]
+const dic = [...set]
   .sort((a, b) => a - b)
   .reduce((a, b, i) => {
     a[b] = i;
     return a;
   }, {});
 
-console.log(answer.map((ele) => array[ele]).join(" "));
+console.log(answer.map((ele) => dic[ele]).join(" "));
