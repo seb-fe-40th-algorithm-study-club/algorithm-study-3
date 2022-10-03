ed3e216c04ebddceaaa045262167e39b2e472ccf
JinhuiKim <public.jinhui.kim@gmail.com>
2022-10-03 21:18:10 +0900

    [BOJ] 11651 좌표 정렬하기 2
    정렬

---
+++ baekjoon-online-judge/6주차/김진희/9단계/08_좌표 정렬하기 2/11651.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let [N, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
+
+input = input
+  .map((ele) => ele.split(" "))
+  .sort((a, b) => {
+    if (a[1] !== b[1]) {
+      return a[1] - b[1];
+    }
+    return a[0] - b[0];
+  })
+  .map((ele) => ele.join(" "))
+  .join("\n");
+
+console.log(input);
